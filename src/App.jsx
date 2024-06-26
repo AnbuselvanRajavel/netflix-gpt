d05d41e1acb7c17922a85646a5f1d8e132011dc3
Anbuselvan R <anbuselvan2310@gmail.com>
2024-06-26 20:03:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold text-green-500'>Hello Everyone! Let's built netflix-gpt
+      </div>
+      </>
+  )
+}
+
+export default App
