7de40533548cd8d8d322e45878d9d56ba5c3bb5a
Anbuselvan R <anbuselvan2310@gmail.com>
2024-06-28 01:04:21 +0530

    CRA && TailwindCSS && Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='text-3xl font-bold text-green-500'>Hello Everyone! Let's built netflix-gpt
+      <div className="text-3xl font-bold text-green-500">
+        Hello Everyone! Let's built netflix-gpt
       </div>
-      </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
